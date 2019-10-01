7189533fb33928a2185f98f8894ac73eee865704
TundePappova <TundePappova@158.197.238.34>
2019-10-01 09:19:56 +0200

    new method "plus"

--- src/main/java/paz1c_tunde/git_test/App.java
+++ src/main/java/paz1c_tunde/git_test/App.java
@@ -1,13 +1,19 @@
 package paz1c_tunde.git_test;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Olika!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("Neviemco");
+		}
+		System.out.println("Notebooky su zle");
+	}
+
+	private static int plus(int a, int b) {
+		return a + b;
+	}
+
 }
